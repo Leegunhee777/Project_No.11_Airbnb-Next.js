71a0dcdbdb712514f3cdde586ab4186cf220c46a
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-04-24 19:55:57 +0900

    feat(bathroom): 욕실정보 등록

--- pages/room/register/bathroom/index.tsx
+++ pages/room/register/bathroom/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NextPage } from 'next';
-
+import RegisterRoomBathroom from '../../../../components/register/RegisterRoomBathroom';
 const bathroom: NextPage = () => {
-  return <div>욕조</div>;
+  return <RegisterRoomBathroom />;
 };
 export default bathroom;
