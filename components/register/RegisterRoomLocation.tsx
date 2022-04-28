26a2a301fa477a8529d6a4583543a94f3f1c1fb8
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-04-29 02:26:21 +0900

    feat(geocode): 구글위치 API 셋팅

--- components/register/RegisterRoomLocation.tsx
+++ components/register/RegisterRoomLocation.tsx
@@ -79,6 +79,23 @@ const RegisterLocation: React.FC = () => {
     dispatch(registerRoomActions.setPostcode(e.target.value));
   };
 
+  //현재 위치 불러오기 성공했을때
+  const onSuccessGetLocation = ({
+    coords,
+  }: {
+    coords: GeolocationCoordinates;
+  }) => {
+    console.log('latitude', coords.latitude);
+    console.log('longitude', coords.longitude);
+  };
+
+  //현재 위치 사용 클릭 시
+  const onClickGetCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(onSuccessGetLocation, e => {
+      console.log(e);
+      alert(e?.message);
+    });
+  };
   return (
     <Container>
       <h2>숙소의 위치를 알려주세요.</h2>
@@ -87,7 +104,12 @@ const RegisterLocation: React.FC = () => {
         정확한 주소는 게스트가 예약을 완료한 후에만 공개됩니다.
       </p>
       <div className="register-room-location-button-wrapper">
-        <Button color="dark_cyan" colorReverse={true} icon={<NavigationIcon />}>
+        <Button
+          color="dark_cyan"
+          colorReverse={true}
+          icon={<NavigationIcon />}
+          onClick={onClickGetCurrentLocation}
+        >
           현재 위치 사용
         </Button>
       </div>
