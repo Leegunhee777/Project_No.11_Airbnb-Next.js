e5768092bb71474c8cbb3cbac3f23656fd924828
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-06-16 02:31:20 +0900

    feat(Checklist): 스텝별 유효한 데이터 항목체크

--- components/register/RegisterRoomChecklist.tsx
+++ components/register/RegisterRoomChecklist.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { isEmpty } from 'lodash';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { useSelector } from '../../store';
 
@@ -16,6 +17,143 @@ const Container = styled.div`
 
 const RegisterRoomChecklist: React.FC = () => {
   const registerRoom = useSelector(state => state.registerRoom);
+
+  //숙소 유형이 활성화 됐는지
+  const isBuildingTypeActiced = useMemo(() => {
+    const { largeBuildingType, buildingType, roomType, isSetUpForGuest } =
+      registerRoom;
+    if (
+      !largeBuildingType ||
+      !buildingType ||
+      !roomType ||
+      isSetUpForGuest === null
+    ) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //숙소 종류가 활성화 됐는지
+  const isRoomTypeActived = useMemo(() => {
+    const {
+      maximumGuestCount,
+      bedroomCount,
+      bedCount,
+      bedList,
+      publicBedList,
+    } = registerRoom;
+    if (
+      !isBuildingTypeActiced ||
+      !maximumGuestCount ||
+      !bedroomCount ||
+      !bedCount
+    ) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //욕실 항목이 활성화됐는지
+  const isBathroomActived = useMemo(() => {
+    const { bathroomCount, bathroomType } = registerRoom;
+    if (!isRoomTypeActived || !bathroomCount || bathroomType === null) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //위치 항목이 활성화됐는지
+  const isLocationActived = useMemo(() => {
+    const {
+      latitude,
+      longitude,
+      country,
+      city,
+      district,
+      streetAddress,
+      detailAddress,
+      postcode,
+    } = registerRoom;
+
+    if (
+      !isBathroomActived ||
+      !latitude ||
+      !longitude ||
+      !country ||
+      !city ||
+      !district ||
+      !streetAddress ||
+      !postcode
+    ) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //편의 시설이 활성화됐는지
+  const isAmentitiesActived = useMemo(() => {
+    const { amentities } = registerRoom;
+
+    if (!isLocationActived || isEmpty(amentities)) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //편의공간 활성화됐는지
+  const isConviniencesActived = useMemo(() => {
+    const { conveniences } = registerRoom;
+
+    if (!isAmentitiesActived || isEmpty(conveniences)) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //사진 항목이 다 채워져있는지
+  const isPhotoActived = useMemo(() => {
+    const { photos } = registerRoom;
+    if (!isConviniencesActived || isEmpty(photos)) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //숙소 설명이 다 채워져있는지
+  const isDescriptionActived = useMemo(() => {
+    const { description } = registerRoom;
+    if (!isPhotoActived || !description) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //숙소 제목이 다 채워져있는지
+  const isTitleActived = useMemo(() => {
+    const { title } = registerRoom;
+    if (!isDescriptionActived || !title) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //숙소 요금이 채워져있는지
+  const isPriceActived = useMemo(() => {
+    const { price } = registerRoom;
+    if (!isTitleActived || !price) {
+      return false;
+    }
+    return true;
+  }, []);
+
+  //예약날짜가 다 채워져있는지
+  const isDateActived = useMemo(() => {
+    const { startDate, endDate } = registerRoom;
+    if (!isPhotoActived || !startDate || !endDate) {
+      return false;
+    }
+    return true;
+  }, []);
   return (
     <Container>
       <p className="register-room-checklist-info">
