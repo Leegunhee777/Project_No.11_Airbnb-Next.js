1567f61ae44c310eec6b8c9956ee0e632536c220
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-06-21 01:46:55 +0900

    feat(Home): 메인화면 검색필터 컴포넌트 셋팅

--- components/home/searchRoomBar/SearchRoomBar.tsx
+++ components/home/searchRoomBar/SearchRoomBar.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import palette from '../../../styles/palette';
+
+import SearchRoomBarLocation from './SearchRoomBarLocation';
+import SearchRoomCheckInDate from './SearchRoomCheckInDate';
+import SearchRoomCheckOutDate from './SearchRoomCheckOutDate';
+import SearchRoomGuests from './SearchRoomGuest';
 
 const Container = styled.div`
   width: 100%;
@@ -8,12 +14,30 @@ const Container = styled.div`
   align-items: center;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.08);
   border-radius: 12px;
+  .search-room-bar-inputs {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    .search-room-bar-input-divider {
+      width: 1px;
+      height: 44px;
+      background-color: ${palette.gray_dd};
+    }
+  }
 `;
 
 const SearchRoomBar: React.FC = () => {
   return (
     <Container>
-      <div className="search-room-bar-inputs"></div>
+      <div className="search-room-bar-inputs">
+        <SearchRoomBarLocation />
+        <div className="search-room-bar-input-divider " />
+        <SearchRoomCheckInDate />
+        <div className="search-room-bar-input-divider " />
+        <SearchRoomCheckOutDate />
+        <div className="search-room-bar-input-divider " />
+        <SearchRoomGuests />
+      </div>
     </Container>
   );
 };
