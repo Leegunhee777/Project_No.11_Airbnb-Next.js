5d257aa9f3624ab3b949431549cff76b33854846
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-07-10 19:10:54 +0900

    feat(RoomDetail): 숙소 이미지 레이아웃 셋팅

--- components/detail/RoomDetail.tsx
+++ components/detail/RoomDetail.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useSelector } from '../../store';
 import palette from '../../styles/palette';
+import RoomDetailPhotos from './RoomDetailPhotos';
 
 const Container = styled.div`
   width: 1120px;
@@ -34,6 +35,7 @@ const RoomDetail: React.FC = () => {
       <p className="room-detail-location">
         {room.district}, {room.city}, {room.country}
       </p>
+      <RoomDetailPhotos />
     </Container>
   );
 };
